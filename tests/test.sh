aedf2cf59dead401cb7626a144b308693e70783f
Eric <eric.t.dawson@gmail.com>
2019-07-22 15:25:42 -0400

    Add names to tests

--- tests/test.sh
+++ tests/test.sh
@@ -10,5 +10,12 @@ svaha=../svaha2
 data=..//data
 
 
-is "$(${svaha} -r ${data}/tgraph.fa -v ${data}/tdel.vcf  | grep -v "^P\|^H" | sort | md5sum | cut -f 1 -d ' ')" "239b9ae8a090f7e79c8f9c65f88a9ceb"
-is "$(${svaha} -r ${data}/tgraph.fa -v ${data}/tdel.vcf -f | grep -v "^P\|^H" | sort | md5sum | cut -f 1 -d ' ')" "717cedc93001238cfbc64dd937ad74a7"
+## Deletion tests
+is "$(${svaha} -r ${data}/tgraph.fa -v ${data}/tdel.vcf  | grep -v "^P\|^H" | sort | md5sum | cut -f 1 -d ' ')" "239b9ae8a090f7e79c8f9c65f88a9ceb" "Graphical deletions are properly produced"
+is "$(${svaha} -r ${data}/tgraph.fa -v ${data}/tdel.vcf -f | grep -v "^P\|^H" | sort | md5sum | cut -f 1 -d ' ')" "717cedc93001238cfbc64dd937ad74a7" "Flat deletions are properly produced"
+
+## Inversion Tests
+
+## Insertion Tests
+
+## Translocation tests
