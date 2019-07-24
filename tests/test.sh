1b46f255d0822adc694f784bfef722d425604c71
Eric <eric.t.dawson@gmail.com>
2019-07-23 21:26:58 -0400

    Update tests

--- tests/test.sh
+++ tests/test.sh
@@ -11,8 +11,8 @@ data=..//data
 
 
 ## Deletion tests
-is "$(${svaha} -r ${data}/tgraph.fa -v ${data}/tdel.vcf -m 128  | grep -v "^P\|^H" | sort | md5sum | cut -f 1 -d ' ')" "239b9ae8a090f7e79c8f9c65f88a9ceb" "Graphical deletions are properly produced"
-is "$(${svaha} -r ${data}/tgraph.fa -v ${data}/tdel.vcf -f -m 128 | grep -v "^P\|^H" | sort | md5sum | cut -f 1 -d ' ')" "717cedc93001238cfbc64dd937ad74a7" "Flat deletions are properly produced"
+is "$(${svaha} -r ${data}/tgraph.fa -v ${data}/tdel.vcf -m 128  | grep -v "^W\|^P\|^H" | sort | md5sum | cut -f 1 -d ' ')" "239b9ae8a090f7e79c8f9c65f88a9ceb" "Graphical deletions are properly produced"
+is "$(${svaha} -r ${data}/tgraph.fa -v ${data}/tdel.vcf -f -m 128 | grep -v "^W\|^P\|^H" | sort | md5sum | cut -f 1 -d ' ')" "717cedc93001238cfbc64dd937ad74a7" "Flat deletions are properly produced"
 
 ## Inversion Tests
 
